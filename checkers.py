721a5ff70fb7d8465aa47339c1dffd4ec81f3e05
GabrielGramlich <mb4510qj@go.minneapolis.edu>
2021-01-14 11:06:50 -0600

    Update refresh spaces

--- checkers.py
+++ checkers.py
@@ -45,7 +45,7 @@ def refresh(lines):
 	for i in range(lines):
 		sys.stdout.write("\033[F") # Cursor up one line
 	for i in range(lines):
-		print(' '*55)	# clears any previously printed characters up end of terminal line
+		print(' '*60)	# clears any previously printed characters up end of terminal line
 	for i in range(lines):
 		sys.stdout.write("\033[F") # Cursor up one line
 	sys.stdout.flush()
